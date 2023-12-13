6e94ad0af91f4d3a0698b34fcca6d38ea7de4d5e
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2023-12-13 21:30:43 +0300

    create user profile widget

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,13 +1,17 @@
-const DashboardLayout = ({ children }: {
-    children: React.ReactNode;
-}) => {
+import { UserButton } from "@clerk/nextjs";
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-screen w-screen relative">
       <aside className="top-0 absolute left-0 h-full border-r w-[200px]">
         daily
       </aside>
       <div className="ml-[200px]">
-        <header className="h-[60px] border-b border-black/10">Daily</header>
+        <header className="h-[60px] border-b border-black/10">
+          <div className="h-full w-full px-6 flex items-center justify-end">
+            <UserButton />
+          </div>
+        </header>
         <div>{children}</div>
       </div>
     </div>
