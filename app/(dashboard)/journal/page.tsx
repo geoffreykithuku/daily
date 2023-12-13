124236c6d4b4aa0800ec03daa1479c100ea118e2
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2023-12-13 21:57:56 +0300

    fetch entries

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,4 +1,26 @@
-const JournalPage = () => {
+import { getUserByClerkID } from "@/utils/auth";
+import prisma from "@/utils/db";
+
+
+const getEntries = async () => {
+    const user = await getUserByClerkID();
+
+    const entries = await prisma.journalEntry.findMany({
+        where: {
+            userId: user.id
+        },
+        orderBy: {
+            createdAt: 'desc'
+        }
+    })
+    return entries;
+}
+
+const JournalPage = async () => {
+
+    const entries = await getEntries();
+    console.log(entries);
+    
   return (
       <div>
         <h1>Journal</h1>
