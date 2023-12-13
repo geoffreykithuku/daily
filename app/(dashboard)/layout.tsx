3327ab7ed9dae47c0869c06be7730bb77c856cf4
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2023-12-13 22:36:57 +0300

    design new entry ui

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -6,13 +6,13 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
       <aside className="top-0 absolute left-0 h-full border-r w-[200px]">
         daily
       </aside>
-      <div className="ml-[200px]">
+      <div className="ml-[200px]  h-full">
         <header className="h-[60px] border-b border-black/10">
           <div className="h-full w-full px-6 flex items-center justify-end">
             <UserButton />
           </div>
         </header>
-        <div>{children}</div>
+        <div className="h-[calc(100vh-60px)]">{children}</div>
       </div>
     </div>
   );
---
+++ components/EntryCard.tsx
@@ -0,0 +1,11 @@
+
+
+const EntryCard = ({entry}) => {
+    return (
+        <div>
+            <h1>{entry.id}</h1>
+        </div>
+    )
+}
+
+export default EntryCard
